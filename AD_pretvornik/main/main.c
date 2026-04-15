a3b8b54e69bcebba407e9b15efb06cc973f72d43
leonbl <leon.blazic@gmail.com>
2026-04-15 12:32:39 +0200

    ADC potenciometer

--- AD_pretvornik/main/main.c
+++ AD_pretvornik/main/main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "driver/gpio.h"
+#include "esp_adc/adc_oneshot.h"
+
+
 
 void app_main(void)
 {
     gpio_reset_pin(16);
     gpio_set_direction(16, GPIO_MODE_OUTPUT);
     gpio_set_level(16, 1);
+
+    adc_oneshot_unit_handle_t adc_handle;
+    adc_oneshot_unit_init_cfg_t init_config = {.unit_id = ADC_UNIT_2};
+    adc_oneshot_new_unit(&init_config, &adc_handle);
+
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_12,
+    };
+    adc_oneshot_config_channel(adc_handle, ADC_CHANNEL_6, &config);
+
     while(1){
-        // praskaj se 
+        int value;
+        adc_oneshot_read(adc_handle, ADC_CHANNEL_6, &value);
+        printf("ADC: %d\n", value);
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
